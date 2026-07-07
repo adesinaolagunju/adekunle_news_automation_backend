035d73342c6d2721704b542653b4d9526453c7a5
adesinaolagunju <adesinaolagunju1@gmail.com>
2026-07-07 19:43:05 +0100

    Made hello world to show in the base url

--- core/urls.py
+++ core/urls.py
@@ -1,12 +1,19 @@
-
 # core/urls.py
 from django.contrib import admin
 from django.urls import path, include
+from django.http import HttpResponse
 from django.views.generic import TemplateView
 from drf_spectacular.views import SpectacularAPIView, SpectacularRedocView
 from drf_spectacular.views import SpectacularSwaggerView
 
+
+def hello_world(request):
+    return HttpResponse("Hello, World!")
+
+
 urlpatterns = [
+    path('', hello_world, name='hello-world'),
+
     path('admin/', admin.site.urls),
     
     # API endpoints
